66010c58dfbf81a76b3a176f8f8f09fc51c4ee90
Ryan Agung Samudra <ryansamudra67@gmail.com>
2022-12-21 03:00:48 +0700

    update JS-3/task5.js

--- week3/JS-3/task5.js
+++ week3/JS-3/task5.js
@@ -1,8 +1,13 @@
 // Buatlah program menggunakan method fetch untuk menampilkan semua name (hanya name nya saja) dari REST API dibawah ini https://jsonplaceholder.typicode.com/users Gunakan debugger console bawaan browser Chrome untuk melihat hasilnya
-
 const loadData = (json) => {
     fetch(json)
-        .then((response) => response.json())
+        .then((response) => {
+            if (response.ok){
+                return response.json()
+            } else {
+                return Promise.reject('Check your API url!')
+            }
+        })
         .then((data) => {
             let output = ''
             let no = 1
@@ -20,5 +25,30 @@ const loadData = (json) => {
             })
             document.querySelector('tbody').innerHTML = output
         })
+        .catch((error) => console.log(`Error : ${error}`))
 }
-loadData("https://jsonplaceholder.typicode.com/users/")
+// loadData("https://jsonplaceholder.typicode.com/users/")
+
+
+// Mas Tova challenge => pake async/await
+const getData = async () => {
+    try {
+
+        // fetching
+        const url = await fetch("https://jsonplaceholder.typicode.com/users/");
+        // tampilkan data
+        let userData = []
+        userData = await url.json();
+        loadUserData(userData)
+
+    } catch (err) {
+        console.log(`Waduhh error nih => ${err}`)
+    }
+}
+const loadUserData = (data) => {
+    const output = data.map((el) => {
+        return el.name
+    })
+    console.log(output.join("\n"))
+}
+// getData()
